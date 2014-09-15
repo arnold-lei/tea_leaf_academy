8be5ee1d9e5c9386fbd188bd6bb32b479b485b8e
Arnold Lei <arnold.lei88@gmail.com>
2014-09-15 16:31:27 -0400

    exercise 1&2 from basics section

---
+++ the_basics/exercise_1.rb
@@ -0,0 +1,4 @@
+FIRST_NAME = "Arnold"
+LAST_NAME = "Lei"
+
+puts "#{FIRST_NAME}" + "#{LAST_NAME}"
---
+++ the_basics/exercise_2.rb
@@ -0,0 +1,6 @@
+num = 7324
+
+puts num / 1000
+puts (num % 1000) / 100
+puts (num %100) / 10
+puts (num %10) 
