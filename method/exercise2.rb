49d084be327909b7326886764e5bb88572d4d186
Arnold Lei <arnold.lei88@gmail.com>
2014-09-17 15:04:42 -0400

    submitting exercises 2-6

---
+++ method/exercise2.rb
@@ -0,0 +1,13 @@
+#What do the following expressions evaluate to
+# x = 2
+# => 2
+# puts x = 2
+# 2
+# => nil
+# p name = "Joe"
+# Joe
+# => Joe
+# four = "four"
+# => "four"
+# print something = "nothing"
+#nothing => nil
---
+++ method/exercise4.rb
@@ -0,0 +1,12 @@
+#What will the following code print to the screen?
+def scream(words)
+  words = words + "!!!!"
+  puts words
+  
+end
+
+scream("Yippeee")
+
+#The following code should return nothing
+
+#The return needs to be put at the end. It will still return nil.
