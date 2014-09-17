6dad3bec54dbd997dac8e160b0e037dcd988c27d
Arnold Lei <arnold.lei88@gmail.com>
2014-09-17 09:49:57 -0400

    submit exercise 5&6

---
+++ variables/exercise_6.rb
@@ -0,0 +1,5 @@
+#NameError: undefined local variable or method `shoes' for main:Object
+  #from (irb):3
+  #from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+
+  #in irb line 3 there is a method or variable that is undefined
