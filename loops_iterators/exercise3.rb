79f93c55c13f5db0192cab6bfce071a490fda6c2
Arnold Lei <arnold.lei88@gmail.com>
2014-09-18 15:56:23 -0400

    submit exercise 3&4

---
+++ loops_iterators/exercise3.rb
@@ -0,0 +1,6 @@
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+
+months.each_with_index do|month, index|
+	puts "#{1 + index}. #{month}"
+end
+
---
+++ loops_iterators/exercise4.rb
@@ -0,0 +1,11 @@
+puts "Choose a number to countdown from"
+num = gets.chomp.to_i
+
+def countdown(num)
+	while num > 0
+		num -= 1
+	 	puts num
+	end
+end
+
+countdown(num)
